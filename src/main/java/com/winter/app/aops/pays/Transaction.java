d6d7a005319c948f8a48ccbc64823ec1bfb8f162
soyoung-jin <jthdud0501@gmail.com>
2024-09-05 11:53:39 +0900

    주석 추가

--- src/main/java/com/winter/app/aops/pays/Transaction.java
+++ src/main/java/com/winter/app/aops/pays/Transaction.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Component;
 @Component
 @Aspect
 //보고 있다가 둘 중 하나가 exception 발생 시 rollback 시켜주는 역할의 클래스
+//굳이 이걸 설정해야 어노테이션을 쓰는건 아님. 따로 설정해주기 위한 클래스~
 public class Transaction {
 	
 	//롤백시켜주는 애들 누구로 할건지 결정함
